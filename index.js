ccab65d2575247069ba44b61b6296fa453cb6896
GavinKZ <joaofslucas2023@gmail.com>
2023-11-28 21:02:13 -0300

    exibição das tarefas na página home concluída

--- index.js
+++ index.js
@@ -44,16 +44,16 @@ app.get('/', (requisicao, resposta) => {
             return console.log(erro)
         }
 
-        const tarefa = dados.map((dado) => {
+        const tarefas = dados.map((dado) => {
             return {
                 id: dado.id,
                 descricao: dado.descricao,
                 completa: dado.completa === 0 ? false : true
             }
         })
-    })
 
-    resposta.render("home")
+        resposta.render("home", { tarefas })
+    })
 })
 
 const conexao = mysql.createConnection({
